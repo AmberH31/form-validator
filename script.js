8bbe3f24b50c73ab629bd8239157949634220a9a
Amber Hsueh <ting820831@gmail.com>
2020-06-17 15:23:51 -0500

    add checkPassword function and done

--- script.js
+++ script.js
@@ -19,9 +19,28 @@ function showSuccess(input) {
 }
 
 //check email vaild
-function validateEmail(email) {
+//old version
+// function validateEmail(email) {
+//   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//   return re.test(String(email).toLowerCase());
+// }
+//new
+function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "Email is not vaild");
+  }
+}
+
+//check passwords match
+function checkPassord(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, "Password do not match");
+  } else {
+    showSuccess(input1, "Passowrds match!");
+  }
 }
 
 //check required fields
@@ -33,6 +52,8 @@ function checkRequired(inputArr) {
         input,
         `${input.id.charAt(0).toUpperCase() + input.id.slice(1)} is required`
       );
+    } else {
+      showSuccess(input);
     }
   });
 }
@@ -65,6 +86,8 @@ form.addEventListener("submit", function (e) {
   //   console.log("click");
   checkLength(username, 5, 15);
   checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPassord(password, password2);
 });
 
 // //Event Listener
